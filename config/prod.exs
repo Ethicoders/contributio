6fa80517927e626542d8d8de028f5e776ee690a4
Laegel <valentin.chouaf@laposte.net>
2021-01-04 16:28:18 +0100

    debug: Changing OTP version

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :contributio, ContributioWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "contributio.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
   # url: [host: "example.com", port: 80],
   # cache_static_manifest: "priv/static/cache_manifest.json"
 
