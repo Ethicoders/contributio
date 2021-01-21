a1d2dbb887b3a360c8a9e9224868cae71c1b708c
Laegel <valentin.chouaf@laposte.net>
2021-01-21 13:27:13 +0100

    fix: Removing duplicated fields

--- priv/repo/migrations/20210113224343_add_gamification.exs
+++ priv/repo/migrations/20210113224343_add_gamification.exs
@@ -1,10 +1,10 @@
-defmodule Contributio.Repo.Migrations.AddUserName do
+defmodule Contributio.Repo.Migrations.AddGamification do
   use Ecto.Migration
 
   def change do
     alter table("users") do
-      add :level, :integer
-      add :current_experience, :integer
+      add :level, :integer, default: 1
+      add :current_experience, :integer, default: 0
     end
   end
 end
