83c1cf3aa713c46a9a292d52b4ae9bd998ea1819
Laegel <valentin.chouaf@laposte.net>
2021-01-21 11:52:06 +0100

    fix: Added missing migration

---
+++ priv/repo/migrations/20201223125311_init_tables.exs
@@ -0,0 +1,27 @@
+defmodule Contributio.Repo.Migrations.AddUserTable do
+  use Ecto.Migration
+
+  def change do
+    create table("users") do
+      add :name, :string
+      add :email, :string
+      add :hash, :string
+      add :token, :string, null: true
+    end
+
+    create table("projects") do
+      add :name, :string
+      add :repo_id, :string
+      add :description, :string
+      add :url, :string
+      add :languages, :map
+    end
+
+    create table("tasks") do
+      add :name, :string
+      add :content, :string
+      add :issue_id, :string
+      add :url, :string
+    end
+  end
+end
